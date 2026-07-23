08cb132db9ccd8a0bb2515505a685907e9ba0d60
youssefmoham655-dev <youssefmoham655@gmail.com>
2026-07-23 19:14:27 +0300

    I added a cube just for testin

--- main.js
+++ main.js
@@ -10,12 +10,21 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 const renderer  = new THREE.WebGLRenderer({antialias: true, canvas});
 const light = new THREE.DirectionalLight(0xFFFFFF, 3);
 const controls = new OrbitControls(camera, canvas);
+const loader = new GLTFLoader();
 
 camera.position.z = 2;
 light.position.set(-1, 2, 4);
 
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material =  new THREE.MeshBasicMaterial();
+const material =  new THREE.MeshBasicMaterial({ color: 0x008000 });
 const cube = new THREE.Mesh(geometry, material);
 
-scene.add(cube, light)
+scene.add(light);
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera)
+}
+
+animate()
